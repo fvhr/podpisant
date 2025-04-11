b8d6ec2641c736f27353d3863c1d73e1f10bacd6
UDanV <danil17092006@gmail.com>
2025-04-12 01:15:03 +0300

    Add Main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import './styles/app.scss';
+import {Main} from "./pages/Main.tsx";
+import {Header} from "./components/header.tsx";
 
 export const App = () => {
   return (
+	  <>
+	  	<Header />
 		<Routes>
-			<Route path="/" element={<div>Главная страница</div>} />
+			<Route path="/" element={<Main />} />
 			<Route path="/about" element={<div>О нас</div>} />
 		</Routes>
+	  </>
   );
 };
