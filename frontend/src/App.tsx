e6693b17a48a8a60d05772673d130ffff18c16db
StanislavRND-2 <nenotice65@gmail.com>
2025-04-12 00:54:44 +0300

    fix frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import './styles/app.scss';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<div>писка</div>} />
-    </Routes>
+		<Routes>
+			<Route path="/" element={<div>Главная страница</div>} />
+			<Route path="/about" element={<div>О нас</div>} />
+		</Routes>
   );
 };
