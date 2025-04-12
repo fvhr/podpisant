43b57cf39107faa034b12cdceab9db616843be2e
UDanV <danil17092006@gmail.com>
2025-04-12 22:07:05 +0300

    added departments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { DocumentDetails, Documents, Login } from './pages';
 import { Main } from './pages/Main.tsx';
 import './styles/app.scss';
 import {Employes} from "./pages/Employes.tsx";
+import {Departments} from "./pages/Departments.tsx";
 
 export const App = () => {
   return (
@@ -10,7 +11,8 @@ export const App = () => {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/main" element={<Main />} />
-      <Route path="/employes" element={<Employes />} />
+      <Route path="/employees" element={<Employes />} />
+      <Route path="/departments" element={<Departments />} />
       <Route path="/documents" element={<Documents />} />
       <Route path="/document" element={<DocumentDetails />} />
     </Routes>
