16d4dcff29285a87f71206af853861f51a9b2e9d
StanislavRND-2 <nenotice65@gmail.com>
2025-04-13 05:47:27 +0300

    add fix

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -4,13 +4,3 @@ export const axiosInstance = axios.create({
   baseURL: 'https://menoitami.ru/api',
   withCredentials: true,
 });
-
-axiosInstance.interceptors.response.use(
-  response => response,
-  async error => {
-    if (error.response?.status === 500) {
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
