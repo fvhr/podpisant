37b6fc8949824bdfc49e9a2961bffb407c699645
Roma <romantkacenko82@gmail.com>
2025-04-13 12:01:33 +0300

    create del user

--- backend/app/database/models/secondaries.py
+++ backend/app/database/models/secondaries.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from enum import StrEnum
 from uuid import UUID
 
-from sqlalchemy import ForeignKey, String, DateTime, func, Enum, BOOLEAN
+from sqlalchemy import ForeignKey, String, DateTime, BOOLEAN
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, relationship, mapped_column
+
 from database.models import Base
 
 
